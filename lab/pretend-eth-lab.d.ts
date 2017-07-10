1e2c97038dcbeaaee1cde2992e10ed4a605a7841
Patrick Medaugh <patrickmedaugh@gmail.com>
2017-07-10 12:08:47 -0600

    merging

---
+++ lab/pretend-eth-lab.d.ts
@@ -0,0 +1,8 @@
+import { EthLab } from "./eth-lab";
+import { MockEthereumClient } from "../src/ethereum-client";
+export declare class PretendEthLab implements EthLab {
+    client: MockEthereumClient;
+    constructor(client: MockEthereumClient);
+    start(): Promise<void>;
+    generate(amount: number): Promise<void>;
+}
---
+++ lab/pretend-eth-lab.ts
@@ -0,0 +1,19 @@
+import {EthLab} from "./eth-lab";
+import {MockEthereumClient} from "../src/ethereum-client";
+
+export class PretendEthLab implements EthLab {
+  client: MockEthereumClient
+
+  constructor(client: MockEthereumClient) {
+    this.client = client
+    client.importAddress('')
+  }
+
+  start(): Promise<void> {
+    return null
+  }
+
+  generate(amount: number): Promise<void> {
+    return this.client.generate('', amount)
+  }
+}

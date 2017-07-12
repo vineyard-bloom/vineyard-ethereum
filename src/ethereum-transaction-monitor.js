1f80b6ba77203e6f2b987ff97deca463ec85fe1b
Dylan Avery <fossilfuelmusic@gmail.com>
2017-07-12 12:24:52 -0600

    Add balance monitor

--- src/ethereum-transaction-monitor.js
+++ src/ethereum-transaction-monitor.js
@@ -36,4 +36,27 @@ var EthereumTransactionMonitor = (function () {
     return EthereumTransactionMonitor;
 }());
 exports.EthereumTransactionMonitor = EthereumTransactionMonitor;
+var EthereumBalanceMonitor = (function () {
+    function EthereumBalanceMonitor(model, ethereumClient, sweepAddress) {
+        this.minimumConfirmations = 2;
+        this.manager = model;
+        this.ethereumClient = ethereumClient;
+        this.sweepAddress = sweepAddress;
+    }
+    EthereumBalanceMonitor.prototype.saveNewTransaction = function (address) {
+        var _this = this;
+        return this.ethereumClient.getBalance(address)
+            .then(function (balance) {
+            return _this.ethereumClient.send(address, _this.sweepAddress, balance)
+                .then(function (transaction) { return _this.manager.saveTransaction(transaction); });
+        });
+    };
+    EthereumBalanceMonitor.prototype.sweep = function () {
+        var _this = this;
+        return this.manager.getAddresses()
+            .then(function (addresses) { return promise_each2_1.each(addresses, function (address) { return _this.saveNewTransaction(address); }); });
+    };
+    return EthereumBalanceMonitor;
+}());
+exports.EthereumBalanceMonitor = EthereumBalanceMonitor;
 //# sourceMappingURL=ethereum-transaction-monitor.js.map

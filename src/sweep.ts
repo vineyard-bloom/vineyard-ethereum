8116d27a1d026f2aafa2732412214ce01be46eeb
Patrick Medaugh <patrickmedaugh@gmail.com>
2017-11-14 13:59:45 -0700

    Debugging gasTransaction

--- src/sweep.ts
+++ src/sweep.ts
@@ -138,12 +138,10 @@ export class Broom {
   }
 
   gasTransaction(abi, address) {
-    const readableAddress = address.slice(2)
-    const readableHotWallet = this.config.hotWallet.slice(2)
     return this.needsGas(abi, address)
       .then(gasLess => {
         if(gasLess) {
-          return this.client.send(readableHotWallet, readableAddress, 0.0003)
+          return this.client.send(this.config.hotWallet, address, 0.0003)
         }
       }).catch(err => console.error(`Error providing gas at address: ${address}:\n ${err}`))
   }

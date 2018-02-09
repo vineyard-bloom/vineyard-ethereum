75617a23792e742719722b45fed6febc08fe176a
Patrick Medaugh <patrickmedaugh@gmail.com>
2018-02-09 11:28:18 -0700

    Still WIP but most methods are at least in place if not in place. Still hasn't been tested.

--- lab/mock-web3.ts
+++ lab/mock-web3.ts
@@ -1,3 +1,4 @@
+import BigNumber from 'bignumber.js'
 import { EthereumTransaction, GethTransaction } from '../src'
 
 export class MockWeb3 {
@@ -7,6 +8,7 @@ export class MockWeb3 {
   constructor () {
     this.eth = new MockEth()
     this.personal = new MockPersonal()
+    this.eth.getAccounts = this.personal.getAccounts // eth not responsible for account generation but for some reason has a read function. Copying it from personal on initialization.
   }
 
   setProvider() {
@@ -17,35 +19,38 @@ export class MockWeb3 {
 export class MockEth {
   transactions: GethTransaction[]
   coinbase: string
+  getAccounts: Function
 
   constructor() {
     this.transactions = []
-    this.coinbase = this.randomAddress()
+    this.coinbase = randomAddress()
   }
 
   getGasPrice() {
-
+    return Math.floor(Math.random() * 1e+6)
   }
 
-  getAccounts() {
-
+  getBalance(address: string, callback: Function) {
+    const creditTxs = this.transactions.filter(tx => tx.to === address)
+    const debitTxs = this.transactions.filter( tx => tx.from === address)
+    const credits = creditTxs.reduce((acc, tx) => tx.value + acc, 0)
+    const debits = debitTxs.reduce((acc, tx) => tx.value + acc, 0)
+    return credits - debits
   }
-
-  getBalance() {
-
-  }
-
-  getBlock(blockIndex, someBoolean, callback) {
-
+  // TODO: Check which functions are async or not
+  getBlock(hashOrNumber: string, includeTxs: boolean, callback: Function) {
+    const getByHash = this.transactions.filter(tx => tx.hash === hashOrNumber)
+    const getByNumber = this.transactions.filter(tx => tx.block === hashOrNumber)
+    const blockTransactions = getByHash || getByNumber
   }
 
   getBlockNumber (callback: Function) {
-    return callback(null, this.randomBlockNumber())
+    return callback(null, randomBlockNumber())
   }
 
   getTransaction (hash: string) {
-    const tx = this.transactions.filter(tx => tx.hash = hash)
-    return tx || this.randomTx()
+    const tx = this.transactions.filter(tx => tx.hash === hash)
+    return tx || randomTx()
   }
 
   getTransactionReceipt (txHash: string, callback: Function) {
@@ -53,43 +58,57 @@ export class MockEth {
   }
 
   sendTransaction(transaction: EthereumTransaction, callback: Function) {
-    const hash = this.randomTxHash()
-    const transactionWithId = Object.assign({ hash: hash, block: this.randomBlockNumber(), status: '1' }, transaction)
+    const hash = randomTxHash()
+    const transactionWithId = Object.assign({ hash: hash, block: randomBlockNumber(), status: '1' }, transaction)
     this.transactions.push(transactionWithId)
     return callback(null, hash)
   }
 
-  private randomTxHash (): string {
-    return Math.random().toString(36).slice(2) + Math.random().toString(36).slice(2) + Math.random().toString(36).slice(2) + Math.random().toString(36).slice(2)
-  }
 
-  private randomBlockNumber (): string {
-    return Math.floor(Math.random() * 1e+6).toString()
-  }
-
-  private randomAddress (): string {
-    return '0x' + Math.random().toString(36).slice(2) + Math.random().toString(36).slice(2)
-  }
-
-  private randomTx (): GethTransaction {
-    return {
-      hash: this.randomTxHash(),
-      to: this.randomAddress(),
-      from: this.randomAddress(),
-      value: Math.floor(Math.random() * 100),
-      block: this.randomBlockNumber(),
-      status: '1'
-    }
-  }
 }
 
 export class MockPersonal {
+  accounts: string[]
+
+  constructor() {
+    this.accounts = []
+  }
 
   unlockAccount(address: string, callback: Function) {
     callback(null, true)
   }
 
   newAccount() {
+    const newAccount = randomAddress()
+    this.accounts.push(newAccount)
+    return newAccount
+  }
 
+  getAccounts() {
+    return Promise.resolve(this.accounts)
+  }
+}
+
+// Random helpers
+function randomTxHash (): string {
+  return Math.random().toString(36).slice(2) + Math.random().toString(36).slice(2) + Math.random().toString(36).slice(2) + Math.random().toString(36).slice(2)
+}
+
+function randomBlockNumber (): string {
+  return Math.floor(Math.random() * 1e+6).toString()
+}
+
+function randomAddress (): string {
+  return '0x' + Math.random().toString(36).slice(2) + Math.random().toString(36).slice(2)
+}
+
+function randomTx (): GethTransaction {
+  return {
+    hash: randomTxHash(),
+    to: randomAddress(),
+    from: randomAddress(),
+    value: new BigNumber(Math.floor(Math.random() * 100)),
+    block: randomBlockNumber(),
+    status: '1'
   }
 }

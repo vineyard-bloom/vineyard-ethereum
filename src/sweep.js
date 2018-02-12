37e0e941e1b6843080b9a769a1ae8e978b20db12
elvece <lvcifferello@gmail.com>
2018-02-12 15:46:23 -0700

    updating sweep to make gas transactions

--- src/sweep.js
+++ src/sweep.js
@@ -1,4 +1,12 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const promise_each2_1 = require("promise-each2");
 const bignumber_js_1 = require("bignumber.js");
@@ -32,13 +40,16 @@ class Broom {
             .then(() => console.log('Finished Ethereum sweep'));
     }
     tokenSweep(abi) {
-        console.log('Starting Token sweep');
-        return this.manager.getDustyAddresses()
-            .then(addresses => {
-            console.log('Dusty token addresses', addresses.length, addresses);
-            return promise_each2_1.each(addresses, (address) => this.tokenSingleSweep(abi, address));
-        })
-            .then(() => console.log('Finished Token sweep'));
+        return __awaiter(this, void 0, void 0, function* () {
+            console.log('Starting Token sweep');
+            yield this.provideGas(abi);
+            return this.manager.getDustyAddresses()
+                .then(addresses => {
+                console.log('Dusty token addresses', addresses.length, addresses);
+                return promise_each2_1.each(addresses, (address) => this.tokenSingleSweep(abi, address));
+            })
+                .then(() => console.log('Finished Token sweep'));
+        });
     }
     tokenSingleSweep(abi, address) {
         return this.tokenContract.getBalanceOf(abi, this.config.tokenContractAddress, address)
@@ -61,7 +72,7 @@ class Broom {
                     return new Error(e);
                 });
             })
-                .catch(e => {
+                .catch((e) => {
                 console.error('Error getting token address balance: ', e.message);
                 return new Error(e);
             });
@@ -80,18 +91,27 @@ class Broom {
         return this.needsGas(abi, address)
             .then(gasLess => {
             if (gasLess) {
-                return this.client.send(address, this.config.tokenContractAddress, 0.0003);
+                const tx = {
+                    from: this.config.hotWallet,
+                    to: address,
+                    value: this.client.toWei(0.0003),
+                    gas: this.config.gas,
+                    gasPrice: this.config.gasPrice
+                };
+                return this.client.sendTransaction(tx).then((result) => {
+                    this.manager.saveGasTransaction({ address: result.to, txid: result.hash });
+                });
             }
         });
     }
     provideGas(abi) {
-        console.log('Starting Salt Gas Provider');
+        console.log('Starting Token Gas Provider');
         return this.manager.getDustyAddresses()
             .then(addresses => {
             console.log('Dusty addresses', addresses.length, addresses);
             return promise_each2_1.each(addresses, (address) => this.gasTransaction(abi, address));
         })
-            .then(() => console.log('Finished Salt Gas Provider job'));
+            .then(() => console.log('Finished Token Gas Provider job'));
     }
     singleSweep(address) {
         return this.client.getBalance(address)

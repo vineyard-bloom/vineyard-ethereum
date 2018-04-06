ed53f4053ea8f51fe17e4185f3efe0f364c87969
Patrick Medaugh <patrickmedaugh@gmail.com>
2018-04-06 11:11:38 -0600

    Update token-client.ts

--- src/token-client.ts
+++ src/token-client.ts
@@ -1,11 +1,23 @@
 import {
   BaseBlock, BlockInfo, ExternalSingleTransaction as ExternalTransaction, FullBlock, ReadClient, Resolve,
-  TransactionStatus
-} from 'vineyard-blockchain/src/types'
+  TransactionStatus, blockchain
+} from 'vineyard-blockchain'
 import { Web3EthereumClientConfig } from './ethereum-client'
 import { Block, EthereumTransaction, Web3TransactionReceipt } from './types'
-import { Web3Client, SendTransaction, Resolve2 } from './client-functions'
-import { initializeWeb3 } from './utility'
+import { 
+  Web3Client, 
+  SendTransaction, 
+  Resolve2, 
+  sendWeb3Transaction,
+  getBlockIndex,
+  getLastBlock,
+  getTransactionStatus,
+  getNextBlockInfo,
+  getFullBlock,
+  getBlock,
+  getTransactionReceipt
+} from './client-functions'
+import { initializeWeb3, getEvents } from './utility'
 
 const Web3 = require('web3')
 const SolidityCoder = require('web3/lib/solidity/coder.js')
@@ -30,228 +42,36 @@ export class TokenClient implements ReadClient<ExternalTransaction> {
     this.abi = this.addAbi(abi)
   }
 
-  async unlockWeb3Account(address: string): Promise<boolean> {
-    return new Promise((resolve: Resolve<boolean>, reject) => {
-      try {
-        this.web3.personal.unlockAccount(address, (err: any, result: boolean) => {
-          if (err) {
-            reject(new Error('Error unlocking account: ' + err.message))
-          } else {
-            resolve(result)
-          }
-        })
-      } catch (error) {
-        reject(new Error('Error unlocking account: ' + address + '.  ' + error.message))
-      }
-    })
-  }
-
   async send(transaction: SendTransaction) {
-    if (!transaction.from) {
-      throw Error('Ethereum transaction.from cannot be empty.')
-    }
-  
-    if (!transaction.to) {
-      throw Error('Ethereum transaction.to cannot be empty.')
-    }
-  
-    return this.unlockWeb3Account(transaction.from)
-      .then(() => {
-        return new Promise((resolve: Resolve2<EthereumTransaction>, reject) => {
-          this.web3.eth.sendTransaction(transaction, (err: any, txid: string) => {
-            if (err) {
-              console.log('Error sending (original)', transaction)
-              reject('Error sending to ' + transaction.to + ': ' + err)
-            } else {
-              const txInfo = this.web3.eth.getTransaction(txid)
-              console.log('Sent Token transaction', txid, txInfo)
-              const transactionResult = Object.assign({}, transaction, {
-                hash: txid,
-                gasPrice: txInfo.gasPrice,
-                gas: txInfo.gas
-              })
-              resolve(transactionResult)
-            }
-          })
-        })
-      })
+    return sendWeb3Transaction(this.web3, transaction)
   }
 
   async getBlockIndex(): Promise<number> {
-    return new Promise((resolve: Resolve<number>, reject) => {
-      this.web3.eth.getBlockNumber((err: any, blockNumber: number) => {
-        if (err) {
-          console.error('Error processing ethereum block number', blockNumber, 'with message', err.message)
-          reject(new Error(err))
-        } else {
-          resolve(blockNumber)
-        }
-      })
-    })
+    return getBlockIndex(this.web3)
   }
 
   async getLastBlock(): Promise<BaseBlock> {
-    let lastBlock: Block = await this.getBlock(await this.getBlockNumber())
-    return {
-      hash: lastBlock.hash,
-      index: lastBlock.number,
-      timeMined: new Date(lastBlock.timestamp * 1000),
-      currency: this.currency
-    }
+    return getLastBlock(this.web3)
   }
 
   async getTransactionStatus(txid: string): Promise<TransactionStatus> {
-    let transactionReceipt: Web3TransactionReceipt = await this.getTransactionReceipt(txid)
-    return transactionReceipt.status.substring(2) === '0' ? TransactionStatus.rejected : TransactionStatus.accepted
+    return getTransactionStatus(this.web3, txid)
   }
 
   async getNextBlockInfo(previousBlock: BlockInfo | undefined): Promise<BaseBlock | undefined> {
-    const nextBlockIndex = previousBlock ? previousBlock.index + 1 : 0
-    let nextBlock: Block = await this.getBlock(nextBlockIndex)
-    if (!nextBlock) {
-      return undefined
-    }
-    return {
-      hash: nextBlock.hash,
-      index: nextBlock.number,
-      timeMined: new Date(nextBlock.timestamp * 1000),
-      currency: this.currency
-    }
+    return getNextBlockInfo(this.web3, previousBlock)
   }
 
-  async getFullBlock(block: BlockInfo): Promise<FullBlock<ExternalTransaction>> {
-    let fullBlock = await this.getBlock(block.index)
-    let blockHeight = await this.getBlockNumber()
-    const filteredTransactions = this.filterTokenTransaction(fullBlock.transactions)
-    const decodedTransactions = await this.decodeTransactions(filteredTransactions)
-    const transactions = decodedTransactions.map(t => ({
-      txid: t.hash,
-      to: t.to,
-      from: t.from,
-      amount: t.value,
-      timeReceived: new Date(fullBlock.timestamp * 1000),
-      confirmations: blockHeight - block.index,
-      block: t.blockNumber,
-      status: t.status
-    }))
-    return {
-      hash: fullBlock.hash,
-      index: fullBlock.number,
-      timeMined: new Date(fullBlock.timestamp * 1000),
-      transactions: transactions
-    }
+  async getFullBlock(blockInfo: BlockInfo): Promise<blockchain.FullBlock<blockchain.ContractTransaction>> {
+    return getFullBlock(this.web3, blockInfo.index)
   }
 
   async getBlock(blockIndex: number): Promise<Block> {
-    return new Promise((resolve: Resolve<Block>, reject) => {
-      this.web3.eth.getBlock(blockIndex, true, (err: any, block: Block) => {
-        if (err) {
-          console.error('Error processing ethereum block', blockIndex, 'with message', err.message)
-          reject(new Error(err))
-        } else {
-          resolve(block)
-        }
-      })
-    })
-  }
-
-  async getBlockNumber(): Promise<number> {
-    return new Promise((resolve: Resolve<number>, reject) => {
-      this.web3.eth.getBlockNumber((err: any, blockNumber: number) => {
-        if (err) {
-          console.error('Error processing ethereum block number', blockNumber, 'with message', err.message)
-          reject(new Error(err))
-        } else {
-          resolve(blockNumber)
-        }
-      })
-    })
+    return getBlock(this.web3, blockIndex)
   }
 
   async getTransactionReceipt(txid: string): Promise<Web3TransactionReceipt> {
-    return new Promise((resolve: Resolve<Web3TransactionReceipt>, reject) => {
-      this.web3.eth.getTransactionReceipt(txid, (err: any, transaction: Web3TransactionReceipt) => {
-        if (err) {
-          console.error('Error querying transaction', txid, 'with message', err.message)
-          reject(err)
-        } else {
-          resolve(transaction)
-        }
-      })
-    })
-  }
-
-  filterTokenTransaction(transactions: Web3Transaction[]) {
-    return transactions.filter(tx => {
-      if (tx && tx.to) {
-        return tx.to.toLowerCase() === this.tokenContractAddress.toLowerCase()
-      }
-    })
-  }
-
-  async decodeTransactions(transactions: any[]) {
-    let decodedTransactions = []
-    for (let t of transactions) {
-      const transaction = await this.web3.eth.getTransactionReceipt(t.hash)
-      if (transaction && transaction.blockNumber && transaction.status === '0x1') {
-        let decoded = this.decodeTransaction(t)
-        t.to = decoded.to
-        t.value = decoded.value
-        decodedTransactions.push(t)
-      }
-    }
-    return decodedTransactions
-  }
-
-  decodeTransaction(transaction: any) {
-    let transferTo
-    let transferValue
-
-    const decodedData = this.decodeMethod(transaction.input)
-    if (decodedData) {
-      const params = decodedData.params
-
-      for (let i = 0; i < params.length; ++i) {
-        if (params[i].name === '_to') {
-          transferTo = params[i].value
-        }
-        if (params[i].name === '_value') {
-          transferValue = params[i].value / 100000000
-        }
-      }
-      return {
-        to: transferTo,
-        value: transferValue
-      }
-    } else {
-      return {
-        to: '',
-        value: 0
-      }
-    }
-  }
-
-  decodeMethod(data: any) {
-    const methodID = data.slice(2, 10)
-    const abiItem = this.methodIDs[methodID]
-    if (abiItem) {
-      const params = abiItem.inputs.map((item: any) => item.type)
-      let decoded = SolidityCoder.decodeParams(params, data.slice(10))
-      return {
-        name: abiItem.name,
-        params: decoded.map((param: any, index: number) => {
-          let parsedParam = param
-          if (abiItem.inputs[index].type.indexOf('uint') !== -1) {
-            parsedParam = new Web3().toBigNumber(param).toString()
-          }
-          return {
-            name: abiItem.inputs[index].name,
-            value: parsedParam,
-            type: abiItem.inputs[index].type
-          }
-        })
-      }
-    }
+    return getTransactionReceipt(this.web3, txid)
   }
 
   addAbi(abiArray: any) {

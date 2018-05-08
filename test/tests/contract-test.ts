f0a6209e707e883dca497a3b893207a98a47352f
Patrick Medaugh <patrickmedaugh@gmail.com>
2018-05-08 10:35:25 -0600

    Tests now condensed into 1 due to ethereum test network setup being way too big and creating too many issues. Tests are passing.

--- test/tests/contract-test.ts
+++ test/tests/contract-test.ts
@@ -14,9 +14,9 @@ const solidityCode2 = fs.readFileSync(__dirname + '/../res/internal-transfer.sol
 const solidityCode3 = fs.readFileSync(__dirname + '/../res/internal-send.sol', 'utf8')
 
 describe('ethereum-contract', function () {
-  this.timeout(2 * minute)
-  
-  it('can detect internal transfers using .call.value()', async function () {
+  this.timeout(4 * minute)
+
+  it('can detect internal transfers', async function () {
     // Setup Ethereum Network
     const config = require('../config/config.json')
     const network = new EthereumNetwork(config.ethereum)
@@ -28,40 +28,40 @@ describe('ethereum-contract', function () {
     const web3 = client.getWeb3()
 
     // Create addresses
-    const address1 = await client.createAddress()
-    const address2 = await client.createAddress()
+    let address1 = await client.createAddress()
+    let address2 = await client.createAddress()
 
     // Populate contract address
     const sendAmount = new BigNumber('124004000010000')
 
     // Create contract
     console.log('block', await client.getWeb3().eth.blockNumber)
-    const output = solc.compile({ sources: { 'internal-call-value.sol': solidityCode1 } }, 1)
-    const parts = output.contracts['internal-call-value.sol:Sender']
-    const abi = parts.interface
-    const bytecode = '0x' + parts.bytecode
-    const contract = web3.eth.contract(JSON.parse(abi))
+    let output = solc.compile({ sources: { 'internal-call-value.sol': solidityCode1 } }, 1)
+    let parts = output.contracts['internal-call-value.sol:Sender']
+    let abi = parts.interface
+    let bytecode = '0x' + parts.bytecode
+    let contract = web3.eth.contract(JSON.parse(abi))
     console.log('block', await client.getWeb3().eth.blockNumber)
-    const gasEstimate = await promisify(web3.eth.estimateGas.bind(web3.eth))({ data: bytecode, from: address1 })
+    let gasEstimate = await promisify(web3.eth.estimateGas.bind(web3.eth))({ data: bytecode, from: address1 })
     await unlockWeb3Account(web3, network.getCoinbase())
-    const txid = await deployContract(web3, {
+    let txid = await deployContract(web3, {
       data: bytecode,
       from: network.getCoinbase(),
       gas: gasEstimate,
       gasPrice: 20000000000,
     })
     await miners[0].mineBlocks(5, 200 * 1000)
-    const receipt = await getTransactionReceipt(web3, txid)
-    const tx = await client.sendTransaction({
+    let receipt = await getTransactionReceipt(web3, txid)
+    let tx = await client.sendTransaction({
       from: network.getCoinbase(),
       to: receipt.contractAddress,
       value: sendAmount
     })
     await miners[0].mineBlocks(5, 200 * 1000)
-    const instance = contract.at(receipt.contractAddress)
+    let instance = contract.at(receipt.contractAddress)
 
-    const data = await instance.send.getData(address2)
-    const contractSendTx = await web3.eth.sendTransaction({
+    let data = await instance.send.getData(address2)
+    let contractSendTx = await web3.eth.sendTransaction({
       from: network.getCoinbase(),
       to: receipt.contractAddress,
       data: data,
@@ -71,53 +71,48 @@ describe('ethereum-contract', function () {
     })
     await miners[0].mineBlocks(5, 200 * 1000)
     assert(web3.eth.getBalance(address2).toString() === '7')
-    const callLog = await client.traceTransaction(contractSendTx)
+    let callLog = await client.traceTransaction(contractSendTx)
     assert(callLog[0].address === address2)
     assert(callLog[0].value === 7)
-  })
 
-  it('can detect internal transfers using .transfer()', async function() {
-  // Create addresses
-    const address1 = await client.createAddress()
-    const address2 = await client.createAddress()
+    // END OF CALL.VALUE TRANSFER
 
-  // Populate contract address
-    const sendAmount = new BigNumber('124004000010000')
+    address1 = await client.createAddress()
+    address2 = await client.createAddress()
 
-  // Create contract
     console.log('block', await client.getWeb3().eth.blockNumber)
-    const output = solc.compile(
+    output = solc.compile(
     { sources: { 'internal-transfer.sol': solidityCode2 } },
     1
   )
-    const parts = output.contracts['internal-transfer.sol:Sender']
-    const abi = parts.interface
-    const bytecode = '0x' + parts.bytecode
-    const contract = web3.eth.contract(JSON.parse(abi))
+    parts = output.contracts['internal-transfer.sol:Sender']
+    abi = parts.interface
+    bytecode = '0x' + parts.bytecode
+    contract = web3.eth.contract(JSON.parse(abi))
     console.log('block', await client.getWeb3().eth.blockNumber)
-    const gasEstimate = await promisify(web3.eth.estimateGas.bind(web3.eth))({
+    gasEstimate = await promisify(web3.eth.estimateGas.bind(web3.eth))({
       data: bytecode,
       from: address1
     })
-    // await unlockWeb3Account(web3, network.getCoinbase())
-    const txid = await deployContract(web3, {
+    await unlockWeb3Account(web3, network.getCoinbase())
+    txid = await deployContract(web3, {
       data: bytecode,
       from: network.getCoinbase(),
       gas: gasEstimate,
       gasPrice: 20000000000
     })
     await miners[0].mineBlocks(5, 200 * 1000)
-    const receipt = await getTransactionReceipt(web3, txid)
-    const tx = await client.sendTransaction({
+    receipt = await getTransactionReceipt(web3, txid)
+    tx = await client.sendTransaction({
       from: network.getCoinbase(),
       to: receipt.contractAddress,
       value: sendAmount
     })
     await miners[0].mineBlocks(5, 200 * 1000)
-    const instance = contract.at(receipt.contractAddress)
+    instance = contract.at(receipt.contractAddress)
 
-    const data = await instance.send.getData(address2, 7)
-    const contractSendTx = await web3.eth.sendTransaction({
+    data = await instance.send.getData(address2, 7)
+    contractSendTx = await web3.eth.sendTransaction({
       from: network.getCoinbase(),
       to: receipt.contractAddress,
       data: data,
@@ -127,63 +122,48 @@ describe('ethereum-contract', function () {
     })
     await miners[0].mineBlocks(5, 200 * 1000)
     assert(web3.eth.getBalance(address2).toString() === '7')
-    const callLog = await client.traceTransaction(contractSendTx)
+    callLog = await client.traceTransaction(contractSendTx)
     assert(callLog[0].address === address2)
     assert(callLog[0].value === 7)
-  })
 
-  it('can detect internal transfers using .send()', async function() {
-  
-  // Setup Ethereum Network
-    const config = require('../config/config.json')
-    const network = new EthereumNetwork(config.ethereum)
-    network.initialize()
-    const miners = await network.createMiners(2)
-    const node = await network.createControlNode()
+    // END OF .TRANSFER TRANSFER
 
-    const client = node.getClient()
-    const web3 = client.getWeb3()
+    address1 = await client.createAddress()
+    address2 = await client.createAddress()
 
-  // Create addresses
-    const address1 = await client.createAddress()
-    const address2 = await client.createAddress()
-
-  // Populate contract address
-    const sendAmount = new BigNumber('124004000010000')
-
-  // Create contract
     console.log('block', await client.getWeb3().eth.blockNumber)
-    const output = solc.compile(
+    output = solc.compile(
     { sources: { 'internal-send.sol': solidityCode3 } },
     1
   )
-    const parts = output.contracts['internal-send.sol:Sender']
-    const abi = parts.interface
-    const bytecode = '0x' + parts.bytecode
-    const contract = web3.eth.contract(JSON.parse(abi))
+    parts = output.contracts['internal-send.sol:Sender']
+    abi = parts.interface
+    bytecode = '0x' + parts.bytecode
+    contract = web3.eth.contract(JSON.parse(abi))
     console.log('block', await client.getWeb3().eth.blockNumber)
-    const gasEstimate = await promisify(web3.eth.estimateGas.bind(web3.eth))({
+    gasEstimate = await promisify(web3.eth.estimateGas.bind(web3.eth))({
       data: bytecode,
       from: address1
     })
-    const txid = await deployContract(web3, {
+    await unlockWeb3Account(web3, network.getCoinbase())
+    txid = await deployContract(web3, {
       data: bytecode,
       from: network.getCoinbase(),
       gas: gasEstimate,
       gasPrice: 20000000000
     })
     await miners[0].mineBlocks(5, 200 * 1000)
-    const receipt = await getTransactionReceipt(web3, txid)
-    const tx = await client.sendTransaction({
+    receipt = await getTransactionReceipt(web3, txid)
+    tx = await client.sendTransaction({
       from: network.getCoinbase(),
       to: receipt.contractAddress,
       value: sendAmount
     })
     await miners[0].mineBlocks(5, 200 * 1000)
-    const instance = contract.at(receipt.contractAddress)
+    instance = contract.at(receipt.contractAddress)
 
-    const data = await instance.send.getData(address2, 7)
-    const contractSendTx = await web3.eth.sendTransaction({
+    data = await instance.send.getData(address2, 7)
+    contractSendTx = await web3.eth.sendTransaction({
       from: network.getCoinbase(),
       to: receipt.contractAddress,
       data: data,
@@ -193,8 +173,11 @@ describe('ethereum-contract', function () {
     })
     await miners[0].mineBlocks(5, 200 * 1000)
     assert(web3.eth.getBalance(address2).toString() === '7')
-    const callLog = await client.traceTransaction(contractSendTx)
+    callLog = await client.traceTransaction(contractSendTx)
     assert(callLog[0].address === address2)
-    assert(callLog[0].value === '7')
+    assert(callLog[0].value === 7) 
+
+    // END OF .SEND TRANSFER
   })
+
 })

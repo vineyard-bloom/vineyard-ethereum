69c480d5e709329ef485725c430777d09613c8a3
Patrick Medaugh <patrickmedaugh@gmail.com>
2018-05-11 13:11:16 -0600

    Fixes to eth-internal test script

--- test/scripts/eth-internal-tester.ts
+++ test/scripts/eth-internal-tester.ts
@@ -1,6 +1,9 @@
 import { Web3EthereumClient } from '../../src'
-
 const ethereumClient = new Web3EthereumClient({ http: 'http://localhost:8545' })
+
+const startIndex = process.argv[2]
+const endIndex = process.argv[3]
+const blocks = []
 
 async function getBlockTransactions(index: number) {
   const block = await ethereumClient.getBlock(index)
@@ -15,4 +18,12 @@ async function getBlockTransactions(index: number) {
     }
     return tx
   })
+  return block
 }
+
+for (let i = startIndex; i <= endIndex; i++) {
+  const block = getBlockTransactions(i)
+  blocks.push(block)
+}
+
+console.log(blocks)

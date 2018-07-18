9c4e3fcc85038294e2096b33beb85e9cb25bfc41
amberjohnsonsmile <amber.johnson.smile@gmail.com>
2018-07-18 12:58:39 -0600

    Populating unit tests in ethereum-client-test

--- test/unit-tests/ethereum-client-test.ts
+++ test/unit-tests/ethereum-client-test.ts
@@ -1,10 +1,41 @@
+import { Web3EthereumClient } from '../../src'
+
 const assert = require('assert')
 const minute = 60 * 1000
+const config = require('../config/config.json')
 
-describe('ethereum client', function () {
+describe('ethereum client', async function () {
   this.timeout(2 * minute)
+  const web3Config = { http: config.ethereum.http }
+  const client = new Web3EthereumClient(web3Config)
 
-  it('', async function () {
-    // Test ethereum client
+  it('can get the highest block index', async function () {
+    const blockIndex = await client.getBlockIndex()
+    assert(blockIndex, 'it should return the highest block index')
   })
+
+  it('can get newest block', async function () {
+    const lastBlock = await client.getLastBlock()
+    assert(lastBlock, 'it should return the newest block on the node')
+    assert(lastBlock.hash, 'the block should contain a hash')
+    assert(lastBlock.index, 'the block should contain an index')
+    assert(lastBlock.timeMined, 'the block should contain timeMined')
+  })
+
+  // getNextBlockInfo
+  it('can get the next block info', async function () {
+    const nextBlock = await client.getBlockIndex()
+    assert(nextBlock, '')
+  })
+
+  // getFullBlock
+  // getTransactionStatus
+  // getTransactionReceipt
+  // getTransaction
+  // fromWei
+  // getAccounts
+  // getBalance
+  // getBlock
+  // getBlockNumber
+  // getGas
 })

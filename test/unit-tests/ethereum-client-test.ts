4610f67459a347565c2ca4c889a425d7b032ff37
amberjohnsonsmile <amber.johnson.smile@gmail.com>
2018-07-18 11:45:13 -0600

    Creating unit test file for ethereum client

---
+++ test/unit-tests/ethereum-client-test.ts
@@ -0,0 +1,10 @@
+const assert = require('assert')
+const minute = 60 * 1000
+
+describe('ethereum client', function () {
+  this.timeout(2 * minute)
+
+  it('', async function () {
+    // Test ethereum client
+  })
+})
